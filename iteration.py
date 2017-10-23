606bf394a02db057aea560da0cc96836b7dbaf80
Trevor Fox <foxtw@s.dcsdk12.org>
2017-10-23 08:07:03 -0600

    Print names with scores

--- iteration.py
+++ iteration.py
@@ -10,3 +10,8 @@ def iterate(list):
     # standard loop with range
     #for i in range(0, len(list)):
         #print list[i]
+
+
+def print_scores(names, scores):
+    for i in range(0, len(names)):
+        print names[i] , " scored " , scores[i]
