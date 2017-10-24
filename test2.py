7e09c065fe3eaf844dd32592473db8b89c35966e
Foxtw1 <33030443+Foxtw1@users.noreply.github.com>
2017-10-23 19:45:00 -0600

    test2.py
    Lists names, grades, and ages, then prints them

---
+++ test2.py
@@ -0,0 +1,18 @@
+from iteration import *
+
+scores = [100, 90, 85, 0, 101]
+
+# iterate(scores)
+
+names = ["Amy", "Bill", "Carol", "Dan", "Emily"]
+
+# iterate(names)
+
+# print_scores(names, scores)
+
+
+ages = ["5", "2", "1/2", "Over 9000", "0"]
+
+# iterate(ages)
+
+print_names_scores_ages(names, scores, ages)
